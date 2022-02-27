d37d05704641bb17767e714f765b929ae5cb679f
Raghav Gupta <ragaws1674@gmail.com>
2022-02-27 14:43:42 +0530

    updated: jira logic

--- scripts/check_status_field.py
+++ scripts/check_status_field.py
@@ -1,14 +1,18 @@
 #!/bin/python3
 import os
-from jira.jira import JiraAPI
 import sys
 sys.path.append('.')
+
+from jira.jira import JiraAPI
+
 
 USERNAME = os.getenv('JIRA_USERNAME')
 API_TOKEN = os.getenv("JIRA_API_TOKEN")
 
+TICKET_NUMBER = os.getenv('JIRA_TICKET_NUMBER')
+FIELD_PATH = os.getenv('FIELD_PATH').split(',')
 
 jira = JiraAPI(USERNAME, API_TOKEN, "config/jira.config.yaml")
 
 
-print(jira.get_field_value_from_issue(["<fields>"], "<TICKET-NUMBER>"))
+print(jira.get_field_value_from_issue(FIELD_PATH, TICKET_NUMBER))
